c80a6d86d36977751c8dd8ec941fb1be75628d49
Yaya T <tang.yaya@gmail.com>
2019-04-03 10:23:14 +0300

    include loop for graphing

--- src/5_visualize-triphase.R
+++ src/5_visualize-triphase.R
@@ -1,25 +1,16 @@
 #  for visualizing data
-# install.packages("viridis")
 library(viridis)
 library(plotly)
 
-irga_days <- read.csv(here::here('results/irga_days.csv'))
-plot_data <- read.csv(here::here('results/tubes_to_plot.csv'))
+imported_data <- read.csv(here::here('results/tubes_to_plot.csv'))
 
-cumul_c <- plot_data %>% 
-    filter(treatment == 'C') %>% 
-    rename(daily_C_gross = infer_samp_perday,
-           cumul_C_gross = cumul_gross,
-           cumul_C_phase = cumul_phase_gross) %>% 
-    select(sampleID, trt_ID, MC, treatment, exp_count, daily_C_gross, cumul_C_gross, cumul_C_phase)
-
-max_p1 <- max(filter(plot_data, phase == 1)$exp_count)
-max_p2 <- max(filter(plot_data, phase == 2)$exp_count)
+max_p1 <- max(filter(imported_data, phase == 1)$exp_count)
+max_p2 <- max(filter(imported_data, phase == 2)$exp_count)
 
 # DAILY GRAPHS
 # Loop for graphing cumulative results by MC---------
 
-graph_data <- plot_data %>% 
+graph_data <- imported_data %>% 
     filter(treatment!='R')
 
 var_to_graph <- c('infer_samp_perday',
@@ -54,42 +45,61 @@ dynamic_data <- tibble(var_to_graph, se_to_graph, y_titles, plot_titles)
 #   5 = cumul phase gross by tube
 #   6 = cumul phase diff by tube
 
-# for (i in seq_along(length(dynamic_data))){
+mcs <- c('BU', 'BG', 'GU', 'GG')
 
-# i is the type of graph, according to the titles above
-i <- 6
-mc_filt <- 'GG'
-selected_data <- graph_data %>%
-    filter(MC == mc_filt) %>%
-    select(sampleID, exp_count, phase, trt_ID, !!dynamic_data$var_to_graph[[i]], 
-           !!dynamic_data$se_to_graph[[i]])
-renamed_data <- selected_data %>% 
-    rename(graph_yvar = !!dynamic_data$var_to_graph[[i]],
-           graph_se = !!dynamic_data$se_to_graph[[i]])
+for (i in seq_along(var_to_graph)){
+    # i is the type of graph, according to the titles above
+    print(i)
 
-plot_data <- renamed_data %>% 
-    ungroup() %>% 
-    rename(Treatment = trt_ID)
-
-any_plot <- ggplot(plot_data, aes(exp_count, graph_yvar, color=Treatment, group=sampleID)) + 
-    geom_line() +
-    geom_vline(xintercept=max_p1, color="grey", size = 0.3) +
-    geom_vline(xintercept=max_p2, color="grey", size = 0.3) +
-    geom_hline(yintercept=0) +
-    geom_point(size=0.5) +
-    geom_errorbar(aes(ymin = graph_yvar - graph_se,ymax = graph_yvar + graph_se), width=0.3) +
-    labs(x="Experimental days lapsed", y=dynamic_data$y_titles[[i]]) +
-    ggtitle(paste(dynamic_data$plot_titles[[i]]))+
-    scale_fill_viridis() +
-    theme(plot.title = element_text(hjust = 0.5),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_line(colour = "black")) 
-
-# any_plot
-p <- ggplotly(any_plot)
-htmlwidgets::saveWidget(as_widget(p), paste0(i, mc_filt, "_index.html"))
-
-# ggsave(paste0('results/',i,'_by_', dynamic_data$graph_group[i], '.png'), width=10, height=8, dpi=600)
-ggsave(paste0('results/',i,'_by_', mc_filt, '.png'), width=10, height=8, dpi=600)
-# }
+    minmax_data <- graph_data %>%
+        select(trt_ID, exp_count, phase, !!dynamic_data$var_to_graph[[i]], 
+               !!dynamic_data$se_to_graph[[i]]) %>% #, !!dynamic_data$graph_group[[i]])
+        rename(vals = !!dynamic_data$var_to_graph[[i]],
+               all_se = !!dynamic_data$se_to_graph[[i]])
+    # mutate(min_sum = vals - all_se)
+    min_y_val <- minmax_data[which(minmax_data$vals == min(minmax_data$vals)),]$vals
+    min_y <- if(min_y_val<0)min_y_val * 1.1 else min_y_val*0.9
+    
+    max_y_val <- minmax_data[which(minmax_data$vals == max(minmax_data$vals)),]$vals
+    max_y <- if(max_y_val<0)max_y_val * 0.9 else max_y_val*1.1
+    
+    for (j in seq_along(mcs)){
+        mc_filt <- mcs[[j]]
+        print(mc_filt)
+        
+        selected_data <- graph_data %>%
+            filter(MC == mc_filt) %>%
+            select(sampleID, exp_count, phase, trt_ID, !!dynamic_data$var_to_graph[[i]], 
+                   !!dynamic_data$se_to_graph[[i]])
+        renamed_data <- selected_data %>% 
+            rename(graph_yvar = !!dynamic_data$var_to_graph[[i]],
+                   graph_se = !!dynamic_data$se_to_graph[[i]])
+        
+        plot_data <- renamed_data %>% 
+            ungroup() %>% 
+            rename(Treatment = trt_ID)
+        
+        any_plot <- ggplot(plot_data, aes(exp_count, graph_yvar, color=Treatment, group=sampleID)) + 
+            ylim(min_y, max_y) +
+            geom_line(aes(group=Treatment)) +
+            geom_vline(xintercept=max_p1, color="grey", size = 0.3) +
+            geom_vline(xintercept=max_p2, color="grey", size = 0.3) +
+            geom_hline(yintercept=0) +
+            geom_point(size=0.5) +
+            geom_errorbar(aes(ymin = graph_yvar - graph_se,ymax = graph_yvar + graph_se), width=0.3) +
+            labs(x="Experimental days lapsed", y=dynamic_data$y_titles[[i]]) +
+            ggtitle(paste(dynamic_data$plot_titles[[i]]))+
+            scale_fill_viridis() +
+            theme(plot.title = element_text(hjust = 0.5),
+                  panel.border = element_blank(),
+                  panel.background = element_blank(),
+                  axis.line = element_line(colour = "black")) 
+        
+        # any_plot
+        p <- ggplotly(any_plot)
+        htmlwidgets::saveWidget(as_widget(p), paste(i, var_to_graph[i], mc_filt, ".html", sep="_"))
+        
+        # ggsave(paste0('results/',i,'_by_', dynamic_data$graph_group[i], '.png'), width=10, height=8, dpi=600)
+        ggsave(paste0('results/',i,'_by.tube_', mc_filt, '.png'), width=10, height=8, dpi=600)
+    }
+}
